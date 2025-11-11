4210e0250b479777503dd653e113aaf7fa7d01e5
kiyo9w <ngokapikapi@gmail.com>
2025-11-11 13:02:52 +0700

    Update freezed syntax to const factory constructors

--- lib/features/demo/bloc/demo_notification.dart
+++ lib/features/demo/bloc/demo_notification.dart
@@ -1,12 +1,12 @@
 part of 'demo_bloc.dart';
 
-@Freezed(equal: false)
-class DemoNotification with _$DemoNotification {
-  factory DemoNotification.insertSuccess({
+@freezed
+abstract class DemoNotification with _$DemoNotification {
+  const factory DemoNotification.insertSuccess({
     required String message,
   }) = _NotificationInsertSuccess;
 
-  factory DemoNotification.insertFailed({
+  const factory DemoNotification.insertFailed({
     required String message,
   }) = _NotificationInsertFailed;
 }
--- lib/features/dog_image_random/bloc/dog_image_random_notification.dart
+++ lib/features/dog_image_random/bloc/dog_image_random_notification.dart
@@ -1,12 +1,12 @@
 part of 'dog_image_random_bloc.dart';
 
-@Freezed(equal: false)
-class DogImageRandomNotification with _$DogImageRandomNotification {
-  factory DogImageRandomNotification.notifySuccess({
+@freezed
+abstract class DogImageRandomNotification with _$DogImageRandomNotification {
+  const factory DogImageRandomNotification.notifySuccess({
     required String message,
   }) = _NotificationNotifySuccess;
 
-  factory DogImageRandomNotification.notifyFailed({
+  const factory DogImageRandomNotification.notifyFailed({
     required String message,
   }) = _NotificationNotifyFailed;
 }
